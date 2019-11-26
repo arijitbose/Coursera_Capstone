5ed2252dd743a4c85564f9784286d0975e89756a
arijitbose <arijitbos@gmail.com>
2019-11-26 21:29:50 +0530

    Hello Capstone Project Course!
    Hello Capstone Project Course!

---
+++ Capstone Project of Coursera.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#This notebook will be used for Capstone project
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
